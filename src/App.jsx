5fa64b7afd61d87b0d4cd94964770b4e715dace5
Antonio Vera <antoniov@bluetrailsoft.com>
2021-07-26 14:45:04 -0500

    Agregando pregunta

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import Pregunta from "./components/Pregunta";
+import React, { useState } from 'react';
 
 function App() {
+
+  const [presupuesto, setPresupuesto] = useState(0);
+  const [restante, setRestante] = useState(0);
+
   return (
     <div className="container">
       <header>
         <h1>Gasto Semanal</h1>
       </header>
       <div className="contenido-principal">
-        <Pregunta/>
+        <Pregunta
+          setPresupuesto={setPresupuesto}
+          setRestante={setRestante}
+        />
       </div>
     </div>
   );
--- src/components/Pregunta.jsx
+++ src/components/Pregunta.jsx
@@ -1,19 +1,47 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import Error from "./Error";
+const Pregunta = ({setPresupuesto, setRestante}) => {
 
-const Pregunta = () => {
+    const [presupuesto, setCantidad] = useState(0);
+    const [error, setError] = useState(false)
+    
+    const agregarPresupuesto = e=>{
+        e.preventDefault();
+
+        if(presupuesto<1 || isNaN(presupuesto)){
+            setError(true);
+            return;
+        }
+
+        setError(false);
+
+        setPresupuesto(presupuesto);
+        setRestante(presupuesto)
+    }
+
     return ( 
         <Fragment>
             <h2>Tu presupuesto:</h2>
-            <input 
-                type="text"
-                className="u-full-width"    
-            />
-            <input 
-                type="submit" 
-                value=""
-                className="button-primary u-full-width"
-                value="Definir Presupuesto" 
-            />
+            {
+                error?
+                    <Error
+                        mensaje="Presupuesto incorrecto"
+                    ></Error>
+                :
+                    null
+            }
+            <form onSubmit={agregarPresupuesto}>
+                <input 
+                    type="number"
+                    className="u-full-width"
+                    onChange={e=>setCantidad(parseInt( e.target.value,10 ))}   
+                />
+                <input 
+                    type="submit"
+                    className="button-primary u-full-width"
+                    value="Definir Presupuesto" 
+                />
+            </form>
 
         </Fragment>
      );
